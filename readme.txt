79822242a178d8f81378626a3eb5031648003186
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-19 21:43:32 +0000

    add version 1.0
    git-svn-id: http://plugins.svn.wordpress.org/user-management-tools/trunk@453140 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== User Management Tools ===
+Contributors: scribu, appthemes
+Tags: admin, category, tag, term, taxonomy, hierarchy, organize, manage
+Requires at least: 3.2
+Tested up to: 3.3
+Stable tag: trunk
+
+Allows super-admins to quickly add users to a particular blog in a multisite installation.
+
+== Description ==
+
+With the current Wordpress administration area, the only way to add existing users to a certain blog is by entering their usernames, one by one. It's very time consuming.
+
+With this plugin, you can do it in bulk, by using the familiar "Change role to..." dropdown:
+
+1. Go to WP Admin -> Users.
+2. Click the "Entire Network" link.
+3. Check the users you want to add.
+4. Choose the new role from the dropdown.
+5. Press "Change".
+
+Note that you have to be a super-admin to be able to do this.
+
+== Screenshots ==
+
+1. "Entire Network" link
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+
---
+++ user-management-tools.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: User Management Tools
+Version: 1.0
+Description: Allows you to bulk-add users to a blog in a multisite install
+Author: AppThemes
+Author URI: http://appthemes.com
+Plugin URI: http://wordpress.org/extend/plugins/user-management-tools/
+Text Domain: user-management-tools
+Domain Path: /lang
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+if ( is_admin() && is_multisite() ) {
+	add_action( 'load-users.php', array( 'User_Management_Tools', 'init' ) );
+}
+
+class User_Management_Tools {
+
+	function init() {
+		if ( !is_super_admin() )
+			return;
+
+		self::handle_promotion();
+
+		if ( isset( $_GET['umt_network'] ) ) {
+			add_action( 'pre_user_query', array( __CLASS__, 'pre_user_query' ), 20 );
+			add_action( 'admin_head', array( __CLASS__, 'fix_role' ) );
+		}
+
+		add_action( 'admin_print_footer_scripts', array( __CLASS__, 'script' ), 20 );
+	}
+
+	function pre_user_query( $user_query ) {
+		global $wpdb;
+
+		$user_query->query_from = "FROM $wpdb->users ";
+		$user_query->query_where = 'WHERE 1=1 ';
+	}
+
+	// http://core.trac.wordpress.org/ticket/18995
+	function fix_role() {
+		foreach ( $GLOBALS['wp_list_table']->items as $item ) {
+			if ( empty( $item->roles ) )
+				$item->roles[] = 'none';
+		}
+	}
+
+	// Need to bypass is_multisite() && !is_user_member_of_blog() check in wp-admin/users.php
+	function handle_promotion() {
+		if ( !(isset($_REQUEST['changeit']) && !empty($_REQUEST['new_role']) && !empty($_REQUEST['users'])) )
+			return;
+
+		check_admin_referer('bulk-users');
+
+		$editable_roles = get_editable_roles();
+		if ( empty( $editable_roles[$_REQUEST['new_role']] ) )
+			wp_die(__('You can&#8217;t give users that role.'));
+
+		$update = 'promote';
+		foreach ( $_REQUEST['users'] as $id ) {
+			$id = (int) $id;
+
+			if ( !$id )
+				continue;
+
+			$user = new WP_User($id);
+			$user->set_role($_REQUEST['new_role']);
+		}
+
+		wp_redirect(add_query_arg('update', $update, 'users.php'));
+		exit();
+	}
+
+	function script() {
+		$current = isset( $_GET['umt_network'] ) ? "class='current' " : '';
+
+		$link_html = "<li class='umt-network'><a $current href='" . admin_url( add_query_arg( 'umt_network', true, 'users.php' ) ) . "'>" . __( 'Entire Network', 'user-management-tools' ) . "</a> |</li>";
+?>
+<script type="text/javascript">
+jQuery(function($){
+	$('.subsubsub').prepend("<?php echo $link_html; ?>");
+
+	if ( $('.umt-network .current').length ) {
+		$('.subsubsub :not(.umt-network) .current').removeClass('current');
+	}
+});
+</script>
+<?php
+	}
+}
+
